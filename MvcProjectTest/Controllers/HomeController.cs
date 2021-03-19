0f7445e539a52c334eb3dc4963973eb8bae29c24
Qixue Ju <P2617175@my365.dmu.ac.uk>
2021-03-19 19:49:07 +0800

    Add project files.

---
+++ MvcProjectTest/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcProjectTest.Models;
+
+namespace MvcProjectTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            NewStoryModel newsStory = new NewStoryModel()
+            {
+                Headline = "Phew what a scorcher",
+                ByLine = "Ron Journo",
+                Content = "People are obsessed with the weather"
+            };
+
+            return View(newsStory);
+        }
+    }
+}
